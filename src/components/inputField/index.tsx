5fcfbc995cb5f29df12201b230800c0e58cce206
Osouzaa <gabrielsouzaalves750@gmail.com>
2024-01-17 10:48:29 -0300

    feat: edit info componentes inputField

--- src/components/inputField/index.tsx
+++ src/components/inputField/index.tsx
@@ -5,15 +5,15 @@ interface InputFieldProps {
   label?: string;
   value?: string;
   type?: string;
-  options?: string[]; // Adicionando propriedade para as opções no caso de input do tipo "radio" ou "select"
   onChange?: (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
   className?: string;
+  disabled?: boolean;
 }
 
-const InputField = ({ label, value, type, onChange, className }: InputFieldProps) => (
+const InputField = ({ label, value, type, onChange, className, disabled }: InputFieldProps) => (
   <C.InputWrapper className={className}>
     <C.Label>{label}</C.Label>
-      <C.Input value={value} type={type} onChange={onChange} />
+      <C.Input value={value} type={type} onChange={onChange} disabled={disabled}/>
   </C.InputWrapper>
 );
 
