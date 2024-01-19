646ff22f0ab9bb192f9410c8d6ab31284dbff917
Osouzaa <gabrielsouzaalves750@gmail.com>
2024-01-19 14:14:07 -0300

    styles: new styles in components

--- src/components/inputField/style.ts
+++ src/components/inputField/style.ts
@@ -2,68 +2,60 @@ import styled from "styled-components";
 
 export const InputWrapper = styled.div`
   position: relative;
-  margin-bottom: 15px;
-  margin-left: 2em;
-  /* width: 45%; */
+  margin-left: 1.4em;
+  width: 100%;
 
-  &.InfoCandidate {
-    width: 80%;
+  &.situacao {
+    position: absolute;
+    left: 30em;
+    top: -0.35em;
   }
 
-  &.column-two {
+  /* &.aaaa {
     grid-column: span 2;
-    grid-row: span 2;
-  }
+  } */
 `;
-
 export const Label = styled.label`
   display: block;
   font-size: 14px;
-  margin-bottom: 5px;
+  margin: 5px 0px;
   color: #333;
   width: 100%;
 `;
 
 export const Input = styled.input`
-  width: 100%;
-  padding: 10px;
+  width: 95%;
+  padding: 8px;
   font-size: 14px;
   border: 1px solid #ccc;
   border-radius: 4px;
   outline: none;
+  box-sizing: border-box;
+  font-size: 14px;
+  color: #75757e;
+  background-color: #fff;
 
   &:focus {
-    border-color: #9f3a3e; /* Adicionei uma cor ao foco do input */
+    border-color: #9f3a3e;
   }
-`;
 
-export const Content = styled.div`
-  display: flex;
-  width: 100%;
-  align-items: center;
-  gap: 2em;
-`;
+  &.inputUF {
+    width: 90%;
+    margin-bottom: 4%;
+  }
+  &.data {
+    color: #75757e;
+  }
 
-export const ContentLabel = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 5px;
+  &.situacao {
+    width: 46.7%;
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    outline: none;
+  }
 
-  font-size: 16px;
-  margin-bottom: 5px;
-  color: #333;
-`;
-
-export const Select = styled.select`
-  width: 100%;
-  padding: 10px;
-  font-size: 14px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  outline: none;
-  font-weight: 400;
-
-  option {
-    font-weight: 400;
+  &.InfoCandidate {
+    width: 87%;
   }
 `;
