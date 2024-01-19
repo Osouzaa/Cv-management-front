d00b63b36919c7cd2016b52144bbb2db5365d1dc
Osouzaa <gabrielsouzaalves750@gmail.com>
2024-01-19 14:40:04 -0300

    feat: apaguei a rota de teste

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ import Curriculum from "../pages/curriculum";
 import { Register } from "../pages/register";
 import { NotFound } from "../pages/notFound";
 import { InfoCandidate } from "../pages/infoCandidate";
-import { ModalExcel } from "../components/modalExcel";
+
 
 const RoutesApp = () => {
   return (
@@ -17,7 +17,6 @@ const RoutesApp = () => {
         <Route path="/candidates" element={<Candidate />} />
         <Route path="/curriculum/:id" element={<Curriculum />} />
         <Route path="/register/:id" element={<InfoCandidate />} />
-        <Route path="/aa" element={<ModalExcel />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
