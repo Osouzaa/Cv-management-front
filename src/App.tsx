1c81cc29aaabdd7cd98ebd8406d1b7f6bf3204ce
Osouzaa <gabrielsouzaalves750@gmail.com>
2024-01-22 10:52:17 -0300

    feat: criado a pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { useLocation } from "react-router-dom";
 import { Header } from "./components/header";
 
 const App = () => {
+  const location = useLocation();
   return (
     <div className="Content">
-      <Header />
+      {location.pathname !== "/login" && <Header />}
     </div>
   );
 };
