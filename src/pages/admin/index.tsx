a845d2770dde9a403bea25e453bb5d841b3171a5
Osouzaa <gabrielsouzaalves750@gmail.com>
2024-01-25 08:17:16 -0300

    feat: filtragem na pagina admin

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -3,7 +3,6 @@ import { HeaderAdmin } from "../../components/headerAdmin";
 import { InputSearch } from "../../components/inputSearch";
 import * as C from "./style";
 import axios from "axios";
-import { ModalUser } from "../../components/modalUser";
 import { format } from "date-fns";
 
 interface UserData {
@@ -18,9 +17,10 @@ interface UserData {
 
 const TelaAdmin = () => {
   const [userData, setUserData] = useState<UserData[]>([]);
-  const [newUser, setNewUser] = useState(false);
+  const [allUserData, setAllUserData] = useState<UserData[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [usersPerPage] = useState(4);
+  const [searchText, setSearchText] = useState("");
 
   const roleMapping: Record<string, string> = {
     technique: "Técnico",
@@ -48,7 +48,9 @@ const TelaAdmin = () => {
           `${import.meta.env.VITE_API_GET_ALL_URL}`,
           config
         );
+
         setUserData(response.data);
+        setAllUserData(response.data);
       } catch (error) {
         console.error("Erro ao obter os dados:", error);
       }
@@ -57,36 +59,39 @@ const TelaAdmin = () => {
     fetchData();
   }, []);
 
+  const handleSearch = (text : string) => {
+    setSearchText(text);
+  };
+
   const indexOfLastUser = currentPage * usersPerPage;
   const indexOfFirstUser = indexOfLastUser - usersPerPage;
   const currentUsers = userData.slice(indexOfFirstUser, indexOfLastUser);
+
+  const filteredUsers = allUserData.filter((user) =>
+    user.name.toLowerCase().includes(searchText.toLowerCase())
+  );
 
   const paginate = (pageNumber: number) => {
     setCurrentPage(pageNumber);
   };
 
-  const toggleModal = () => {
-    setNewUser(!newUser);
-  };
-
   return (
     <>
       <HeaderAdmin />
-      <InputSearch />
-      <C.RegisterUser onClick={toggleModal}>Cadastrar usuario</C.RegisterUser>
+      <InputSearch onSearch={handleSearch} />
       <C.Table>
         <thead>
           <C.CabecalhoTable>
             <th>Nome</th>
             <th>E-mail</th>
-            <th>Matricula</th>
+            <th>Matrícula</th>
             <th>Setor</th>
             <th>Status</th>
             <th>Criado em</th>
           </C.CabecalhoTable>
         </thead>
         <C.TableBody>
-          {currentUsers.map((user) => (
+          {filteredUsers.map((user) => (
             <tr
               key={user.id}
               onClick={() => console.log("Detalhes do Usuário:", user)}
@@ -102,7 +107,7 @@ const TelaAdmin = () => {
         </C.TableBody>
       </C.Table>
       <C.Pagination>
-        {[...Array(Math.ceil(userData.length / usersPerPage)).keys()].map(
+        {[...Array(Math.ceil(filteredUsers.length / usersPerPage)).keys()].map(
           (number, index) => (
             <C.PaginationItem
               key={number}
@@ -114,7 +119,7 @@ const TelaAdmin = () => {
           )
         )}
       </C.Pagination>
-      {newUser && <ModalUser onclose={toggleModal} />}
+      
     </>
   );
 };
