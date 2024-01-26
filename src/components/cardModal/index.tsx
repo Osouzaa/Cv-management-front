52b327fdfa9557de20d460f3b118a56fc6c42fcd
Osouzaa <gabrielsouzaalves750@gmail.com>
2024-01-26 13:51:55 -0300

    feat: novas funções em cardModal

--- src/components/cardModal/index.tsx
+++ src/components/cardModal/index.tsx
@@ -1,23 +1,44 @@
-
+import React, { useRef } from "react";
 import * as C from "./style";
 
 interface ICardProps {
-    title: string;
-    img: string;
-    alt: string;
-    onClick?: () => void ;
+  title: string;
+  img: string;
+  alt: string;
+  onClick?: () => void;
+  onFileSelect?: (file: FileList | null) => void;
 }
 
+const CardModal = ({ img, title, alt, onClick, onFileSelect }: ICardProps) => {
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-const CardModal = ({img, title, alt, onClick}: ICardProps) => {
+  const handleFileSelect = () => {
+    if (fileInputRef.current && onFileSelect) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (onFileSelect) {
+      onFileSelect(e.target.files);
+    }
+  };
+
   return (
     <C.ContentImports>
       <div>
         <C.Paragraph>{title}</C.Paragraph>
       </div>
-      <C.Buttons>
-        <img src={img} alt={alt} onClick={onClick}/>
+      <C.Buttons onClick={onClick || handleFileSelect}>
+        <img src={img} alt={alt} />
       </C.Buttons>
+      <input
+        type="file"
+        ref={fileInputRef}
+        style={{ display: "none" }}
+        onChange={handleInputChange}
+        accept=".xlsx"
+      />
     </C.ContentImports>
   );
 };
