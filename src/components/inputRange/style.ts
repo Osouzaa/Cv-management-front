9ca448260e95c4fe88135813714d8fb69a5bca49
Osouzaa <gabrielsouzaalves750@gmail.com>
2024-01-30 14:25:03 -0300

    styles: style in label

---
+++ src/components/inputRange/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const ContentLabel = styled.label`
+  display: block;
+  font-size: 16px;
+  color: #333;
+  width: 100%;
+  text-align: center;
+`;
