04336f4f05ad917b0ee543ac89d9b0cccef6e818
Osouzaa <gabrielsouzaalves750@gmail.com>
2024-01-31 18:51:55 -0300

    feat: new Rotes

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,10 +6,10 @@ import User from "../../image/user.svg";
 import UserBlack from "../../image/user_black.svg";
 import Banco from "../../image/banco_de_dados.svg";
 import BancoBlack from "../../image/banco_de_dados_black.svg";
-import Habilidade from "../../image/habilidade_tecnica.svg";
-import HabilidadeBlack from "../../image/habilidade_tecnica_black.svg";
 import admin from "../../image/admin.svg";
 import admin_black from "../../image/admin_black.svg";
+import icon_black from "../../image/icon_black.svg"
+import icon_white from "../../image/icon_white.svg"
 
 import * as C from "./style";
 
@@ -18,7 +18,7 @@ const Header = () => {
   const isTutorial = location.pathname === "/tutorial";
   const isRegister = location.pathname === "/register";
   const isBank = location.pathname === "/candidates";
-  const isTech = location.pathname === "/tech";
+  const isRated = location.pathname === "/Lista";
   const isAdmin = location.pathname === "/admin";
 
   const navigate = useNavigate();
@@ -80,6 +80,14 @@ const Header = () => {
       >
         <img src={isBank ? Banco : BancoBlack} alt="Icone de Usuario" />
         <p className={isBank ? "active" : "other"}>Banco de candidatos</p>
+      </C.Content>
+
+      <C.Content
+        className={isRated ? "active" : "other"}
+        onClick={() => handleButtonClick("/Lista")}
+      >
+        <img src={isRated ? icon_white : icon_black} alt="Icone de Usuario" />
+        <p className={isRated ? "active" : "other"}>Não avaliados</p>
       </C.Content>
       {userRole === "admin" && (
         <C.Content className={isAdmin ? "active" : "other"} onClick={() => handleButtonClick('/admin')}>
