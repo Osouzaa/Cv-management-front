fdb42a77cd9d4314dbf2136b792f010bc9e78b3f
Osouzaa <gabrielsouzaalves750@gmail.com>
2024-02-06 15:25:07 -0300

    styles: atualizando estilos

--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -11,7 +11,7 @@ export const Container = styled.div`
   background: #fff;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
 
-  @media (max-width: 390px) {
+  @media (max-width: 450px) {
     display: none;
   }
 `;
